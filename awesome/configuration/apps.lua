7498244a86c9d462551eb6133ded5aa22a620b12
omthakare16 <omthakare1606@gmail.com>
2022-07-09 13:05:13 +0530

    intital commit

---
+++ awesome/configuration/apps.lua
@@ -0,0 +1,45 @@
+local filesystem = require('gears.filesystem')
+
+-- Thanks to jo148 on github for making rofi dpi aware!
+local with_dpi = require('beautiful').xresources.apply_dpi
+local get_dpi = require('beautiful').xresources.get_dpi
+local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi -run-command "/bin/bash -c -i \'shopt -s expand_aliases; {cmd}\'"'
+
+return {
+  -- List of apps to start by default on some actions
+  default = {
+    terminal = 'kitty',
+    rofi = rofi_command,
+    lock = 'i3lock-fancy',
+    quake = 'kitty',
+    screenshot = 'flameshot screen -p ~/Pictures',
+    region_screenshot = 'flameshot gui -p ~/Pictures',
+    delayed_screenshot = 'flameshot screen -p ~/Pictures -d 5000',
+    browser = 'brave-browser',
+    editor = 'code', -- gui text editor
+    social = 'flatpak run com.discordapp.Discord',
+    game = rofi_command,
+    files = 'nautilus',
+    music = 'spotify' 
+  },
+  -- List of apps to start once on start-up
+  run_on_start_up = {
+    'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
+    'nm-applet --indicator', -- wifi
+    'pnmixer', -- shows an audiocontrol applet in systray when installed.
+    --'blueberry-tray', -- Bluetooth tray icon
+    'numlockx on', -- enable numlock
+    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    'xfce4-power-manager', -- Power manager
+     'flameshot',
+     'synology-drive -minimized',
+    'feh --randomize --bg-fill ~/.wallpapers/*',
+    '/usr/bin/variety',
+    -- Add applications that need to be killed between reloads
+    -- to avoid multipled instances, inside the awspawn script
+    '~/.config/awesome/configuration/awspawn', -- Spawn "dirty" apps that can linger between sessions
+    'brightness-controller',
+    'libinput-gestures-setup start'
+    
+  }
+}
